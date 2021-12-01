be3a145e5c24f48a5d932aa130922aec3cb76960
Charlie <95353746+Ch-arlie7@users.noreply.github.com>
2021-12-01 20:05:53 +0000

    Update d1.py

--- d1.py
+++ d1.py
@@ -1,16 +1,16 @@
 def d1():
     c1 = 0  
-    prev = False   
-    window = []
-    c2 = 0          
-    with open('d1.txt', 'r') as f:
-        for line in f:   
+    c2 = 0 
+    previous = False   
+    window = []        
+    with open('d1.txt', 'r') as file:
+        for line in file:   
             line = int(line)
             
             ## Part One
-            if prev and line > prev:
+            if previous and line > previous:
                 c1 += 1
-            prev = line
+            previous = line
             
             ## Part Two
             if len(window) == 3:
@@ -22,9 +22,3 @@ def d1():
             else:
                 window.append(line)
     return c1, c2
-
-        
-
-
-
-
